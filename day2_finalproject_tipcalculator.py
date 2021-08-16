b24aac7ea55c5124510e4f167f652307e7b8fe51
Grant <88602977+KootenayCode@users.noreply.github.com>
2021-08-15 18:15:22 -0700

    Update day2_finalproject_tipcalculator.py

--- day2_finalproject_tipcalculator.py
+++ day2_finalproject_tipcalculator.py
@@ -5,7 +5,7 @@ total_bill = input("What was the total bill? ")
 tip_amount = input("How much would you like to tip? 10, 12, 15 or 18? ")
 
 
-#some bounds on tipping 
+#some bounds on tipping
 if(int(tip_amount)<0):
     print("Come on you have to tip something")
 elif(int(tip_amount)>total_bill):
@@ -13,6 +13,7 @@ elif(int(tip_amount)>total_bill):
 
 ppl_splitting = input("How many people are you splitting the bill with? ")
 
+print("**********************************")
 tip = float(tip_amount) / 100
 tip_amount_sum = 1 + tip
 
@@ -24,4 +25,4 @@ indvl_amount = float(total_bill) * float(tip_amount_sum) / int(ppl_splitting)
 print(f"Each person should pay: $ {indvl_amount:.2f}")
 
 #to clean it up, you can remove another line of "final_amount" and input it like
-#it is shown above. :.#f 
+#it is shown above. :.#f
